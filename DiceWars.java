ffda123e87c0931ce52200d267d10de913fcf8d7
Shibe <raulserban05@gmail.com>
2023-10-12 00:02:21 +0200

    Initial commit

---
+++ DiceWars.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+class DiceFrame {
+
+}
+
+class DicePanel {
+    
+}
+
+public class DiceWars {
+    public static void main(String[] args) {
+
+    }
+}
